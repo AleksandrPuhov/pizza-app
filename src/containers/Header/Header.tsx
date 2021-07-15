3b4cc6ac15991ea8bbaf242b73de03878a6a4a75
AleksandrPuhov <aleksandrpuhov123@gmail.com>
2021-07-15 22:59:57 +0300

    Worked on the navbar

--- src/containers/Header/Header.tsx
+++ src/containers/Header/Header.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef, useState } from 'react';
+
 import Wrapper from '../Wrapper/Wrapper';
 import Logo from '../../components/Logo/Logo';
 import Person from '../../components/Person/Person';
@@ -5,18 +7,50 @@ import Nav from '../../components/Nav/Nav';
 import Cart from '../../components/Cart/Cart';
 
 const Header = () => {
+    const [scrollY, setScrollY] = useState(0);
+
+    const [menuFixed, setMenuFixed] = useState(false);
+
+    const refHeaderBottom = useRef<HTMLDivElement>(null);
+
+    const scrollEvent = () => {
+        setScrollY(window.pageYOffset);
+
+        if (refHeaderBottom.current !== null) {
+            setMenuFixed(scrollY >= refHeaderBottom.current.offsetTop);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', scrollEvent);
+        return () => {
+            window.removeEventListener('scroll', scrollEvent);
+        };
+    });
+
+    const classNameCalc =
+        'Header-bottom__inner' + (menuFixed ? ' Header--fixed' : '');
+
     return (
         <header className="Header">
-            <Wrapper>
-                <div className="Header-top">
-                    <Logo />
-                    <Person />
+            <div className="Header-top">
+                <Wrapper>
+                    <div className="Header-top__inner">
+                        <Logo />
+                        <Person />
+                    </div>
+                </Wrapper>
+            </div>
+            <div className="Header-bottom" ref={refHeaderBottom}>
+                <div className={classNameCalc}>
+                    <Wrapper>
+                        <div className="Header-navbar">
+                            <Nav />
+                            <Cart />
+                        </div>
+                    </Wrapper>
                 </div>
-                <div className="Header-bottom">
-                    <Nav />
-                    <Cart />
-                </div>
-            </Wrapper>
+            </div>
         </header>
     );
 };
